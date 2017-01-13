76733614c4eb9cc12ac5e0ebdba330802ce3e352
jhong <jennxhong@gmail.com>
2017-01-13 11:45:55 -0800

    updated sermons page to have themes

--- assets/js/sermons-json-to-table.js
+++ assets/js/sermons-json-to-table.js
@@ -14,6 +14,7 @@ var trWriter = function(rowIndex, record, columns, cellWriter) {
               <td><a href='record.mp3' target='_blank'>record.sermon</a></td>\
               <td><a href='http://www.biblegateway.com/passage/?search=record.scripture' target='_blank'>record.scripture</a></td>\
               <td>record.speaker</td>\
+              <td>record.sermonTheme</td>\
               <td align='center'><a href='record.pdf' target='_blank'><i class='fa fa-file'></i></a></td>\
             </tr>";
   return interpolate(tr, record);
