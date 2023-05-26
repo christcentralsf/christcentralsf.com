e25ac5ae150161a539e7e662d5f95b48c6487cc3
John Choi <jyc1264@gmail.com>
2023-05-26 15:46:05 -0700

    remove sermon links

--- assets/js/sermons-json-to-table.js
+++ assets/js/sermons-json-to-table.js
@@ -11,7 +11,7 @@ var trWriter = function(rowIndex, record, columns, cellWriter) {
   var tr = "<tr>\
               <td>record.date</td>\
               <td class='hidden'>record.sortDate</td>\
-              <td><a href='record.mp3' target='_blank'>record.sermon</a></td>\
+              <td>record.sermon</td>\
               <td><a href='http://www.biblegateway.com/passage/?search=record.scripture' target='_blank'>record.scripture</a></td>\
               <td>record.speaker</td>\
               <td>record.series</td>\
